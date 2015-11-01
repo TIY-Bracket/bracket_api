ae2dbdadaa3b03a159097f078b708db93faaebcf
Jermaine Kee <godson919@icloud.com>
2015-11-01 15:51:55 -0500

    test

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ def hello(request):
                 <h1>Deployment Check</h1>
                 <h2>Glenn checking in from Linux.</h2>
                 <h2>Glenn checking in from Mac.</h2>
+                <h2>Jermaine checking in from Mac.</h2>
                 </body>
             </html>"""
     return HttpResponse(html)
