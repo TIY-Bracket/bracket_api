01afb1aa31be17a224e20c95e5137ef26945c4ec
Karthik <karthik.kasala@gmail.com>
2015-11-02 03:27:30 -0500

    added name for deployment to Heroku

--- api/views.py
+++ api/views.py
@@ -11,6 +11,8 @@ def hello(request):
                 <h2>Glenn checking in from Mac.</h2>
                 <h2>Jermaine checking in from Mac.</h2>
                 <h2>Tyler checking in from Mac.</h2>
+                <h2>Karthik checking in from Mac.</h2>
+
                 </body>
             </html>"""
     return HttpResponse(html)
