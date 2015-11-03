e0cc972ad2614393ed0d2763ad10e9b82d90929b
Jermaine Kee <godson919@icloud.com>
2015-11-02 19:50:09 -0500

    bracket model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Bracket(models.Model):
+    bracket_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    start_dt = models.DateField(blank=True, null=True)
+    end_dt = models.DateField(blank=True, null=True)
