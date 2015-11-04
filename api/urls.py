e21ef2f002adc95ef0602be85be1d0d5f065234a
Glenn Hurley <glenn.hurley@gmail.com>
2015-11-03 19:17:18 -0500

    Added swagger support.

--- api/urls.py
+++ api/urls.py
@@ -14,5 +14,6 @@ router.register(r'position', views.PositionViewSet)
 urlpatterns = [
     url(r'^$', views.bracket_view, name='bracket_view'),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
