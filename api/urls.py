2631edbaf7a6cbc0dc9fa130b8781eeed0a479c3
Karthik <karthik.kasala@gmail.com>
2015-11-05 16:07:23 -0500

    reformatted base.html

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views as views
 
 urlpatterns = [
-    url(r'^$', views.home, name='base.html'),
-    # url(r'^$', views.bracket_view, name='bracket_view'),
-
+    # url(r'^home/', views.landing_page, name='landing_page'),
+    url(r'^$', views.bracket_view, name='bracket_view'),
+    url(r'^home/', views.base_page, name='base'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-def home(request):
+#
+#
+def base_page(request):
     return render(request, 'api/base.html')
+
+#
+def bracket_view(request):
+    return render(request, 'api/bracket_view.html')
