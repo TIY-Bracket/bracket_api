bd6765dd78a316608f62633b7e673c3689ef2966
Karthik <karthik.kasala@gmail.com>
2015-11-06 12:31:21 -0500

    fixing issues with styling

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,7 @@ router.register(r'position', views.PositionViewSet)
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
+    url(r'^bracket_view/', views.bracket_view, name='bracket_view'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
