b737e6cc90d0aeccb3325000f2dc580b254e6828
Karthik <karthik.kasala@gmail.com>
2015-11-15 19:03:28 -0500

    modal styling

--- api/static/facebook-auth-jquery.js
+++ api/static/facebook-auth-jquery.js
@@ -1,4 +1,3 @@
-<script>
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '1643056762643134',
@@ -14,4 +13,3 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script>
